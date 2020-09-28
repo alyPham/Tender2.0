c515721d3d6436934fe3c32fa007fc81877c68fa
alyPham <apham3@macalester.edu>
2020-09-28 11:51:24 -0500

    welcome pages added pt.2 electric boogaloo

--- app/src/androidTest/java/com/example/tender/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/tender/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.tender", appContext.getPackageName());
+        assertEquals("com.example.tender_screen", appContext.getPackageName());
     }
 }
