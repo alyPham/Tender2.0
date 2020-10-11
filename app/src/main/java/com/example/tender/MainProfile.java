49a6b69debf48a988fa20afdec9046ee3d82ed0e
Alex Li <xli@macalester.edu>
2020-10-11 04:20:55 -0500

    Edited Restaurant class so it's usable. Created a few hardcoded restaurant objects and dish objects for now. Created two lists to hold liked and disliked dishes. Gestures with only slight movements wouldn't be responded. Created currentDish object to hold current dish displayed on the screen and locate it in the general dish list. Default profile page should be a random dish from the list. Added two methods likeCurrentDish adn dislikeCurrentDish that performances everything I can think of to be done after swiping left&swiping right gestures. DishManager is half way done. Asynchronous issue has occurred and will ask Lauren&preceptor.

--- app/src/main/java/com/example/tender/MainProfile.java
+++ app/src/main/java/com/example/tender/MainProfile.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class MainProfile extends AppCompatActivity {
     float x1, x2, y1, y2;
@@ -19,18 +20,27 @@ public class MainProfile extends AppCompatActivity {
     ImageView dishImageView;
     TextView nameTextView, dishDescriptionView, distanceView;
 
+    List<Dish> dishListTemp;
+    List<Restaurant> restaurantsListTemp;
+    List<Dish> swipeLeftDishes; //dishlikes
+    List<Dish> likedDishes;
+
     List<Dish> dishList;
     DishManager dishManager;
     Map<String, ArrayList<String>> dishMap;
+    Dish currentDish;
+    Dish currentRestaurant;
 
-//    Dish dish;
+    Restaurant theNeighborhoodCafe;
+    Restaurant shish;
+    Restaurant indochin;
+    Dish countryFriedSteakAndEggs;
+    Dish chickenShawarma;
+    Dish rareBeefPho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        databaseReference = FirebaseDatabase.getInstance().getReference().child("Dish");
-
 
         setContentView(R.layout.food_profile);
 
@@ -39,16 +49,21 @@ public class MainProfile extends AppCompatActivity {
         dishDescriptionView = findViewById(R.id.dishdescription);
         distanceView = findViewById(R.id.distance);
 
+        dishListTemp = new ArrayList<>();
+        restaurantsListTemp = new ArrayList<>();
+        swipeLeftDishes = new ArrayList<>();
+        likedDishes = new ArrayList<>();
+
         dishList = new ArrayList<>();
         dishManager = new DishManager();
         dishMap = new HashMap<>();
 
-        System.out.println("--------------------The list that dishmanager generates is : " +
-                "\n --------------------" + dishManager.getDishes());
+        dishList = new ArrayList<>();
 
-//        setProfile(dishManager.getDishes().get(0));
-//        initializeDishList();
-//        initializeDefaultProfilePage();
+//        System.out.println("--------------------The list that dishmanager generates is : " +
+//                "\n --------------------" + dishManager.getDishes());
+        initializeHardCodeDishNRest();
+        initializeDefaultProfilePage();
     }
 
 
@@ -61,10 +76,77 @@ public class MainProfile extends AppCompatActivity {
      * @param dish displayed on the profile page
      */
     public void setProfile(Dish dish) {
+        currentDish = dish;
         dishImageView.setImageResource(dish.getImgID());
         nameTextView.setText(dish.getName());
         dishDescriptionView.setText(dish.getDescription());
         distanceView.setText(dish.getDistance());
+    }
+
+    public void initializeHardCodeDishNRest() {
+        indochin =new Restaurant(
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantName),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantHours),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantDineIn),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantTakeout),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantDelivery),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantWebsite),
+                convertStrIdtoString(R.string.indochinVietnameseRestaurantPhoneNum)
+        );
+        theNeighborhoodCafe = new Restaurant(
+                getString(R.string.NCname),
+                convertStrIdtoString(R.string.theNeighborhoodCafeHours),
+                convertStrIdtoString(R.string.theNeighborhoodCafeDineIn),
+                convertStrIdtoString(R.string.theNeighborhoodCafeTakeout),
+                convertStrIdtoString(R.string.theNeighborhoodCafeDelivery),
+                convertStrIdtoString(R.string.theNeighborhoodCafeWebsite),
+                convertStrIdtoString(R.string.theNeighborhoodCafePhoneNum)
+        );
+        shish = new Restaurant(
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantName),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantHours),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantDineIn),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantTakeout),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantDelivery),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantWebsite),
+                convertStrIdtoString(R.string.ShishMediterraneanRestaurantPhoneNum)
+        );
+
+        countryFriedSteakAndEggs = new Dish(R.drawable.countryfriedsteak,
+                getString(R.string.countryFriedSteakAndEggsName),
+                convertStrIdtoString(R.string.countryFriedSteakAndEggsDescription),
+                convertStrIdtoString(R.string.countryFriedSteakAndEggsDistance),
+                theNeighborhoodCafe);
+        chickenShawarma = new Dish(R.drawable.chickenshawarma,
+                convertStrIdtoString(R.string.chickenShawarmaName),
+                convertStrIdtoString(R.string.chickenShawarmaDescription),
+                convertStrIdtoString(R.string.chickenShawarmaDistance),
+                shish);
+        rareBeefPho = new Dish(R.drawable.chickenshawarma,
+                convertStrIdtoString(R.string.chickenShawarmaName),
+                convertStrIdtoString(R.string.chickenShawarmaDescription),
+                convertStrIdtoString(R.string.chickenShawarmaDistance),
+                shish);
+        initializeDishListTemp();
+        initializeRestaurantListTemp();
+
+    }
+
+    /**
+     * creates a default temporary dish list. Test only.
+     * TODO: Should be deleted later if firestore is implemented successfully.
+     */
+    public void initializeDishListTemp() {
+        dishListTemp.add(countryFriedSteakAndEggs);
+        dishListTemp.add(chickenShawarma);
+        dishListTemp.add(rareBeefPho);
+    }
+
+    public void initializeRestaurantListTemp() {
+        restaurantsListTemp.add(theNeighborhoodCafe);
+        restaurantsListTemp.add(shish);
+        restaurantsListTemp.add(indochin);
+
     }
 
     /**
@@ -72,28 +154,32 @@ public class MainProfile extends AppCompatActivity {
      */
     //TODO: Should read in data from firebase and
     //      generates lists in random orders.
-    public void initializeDishList() {
-        // hardcoded for now but will be changed into random auto-generated lists
-//        dishList.add(new Dish(R.drawable.countryfriedsteak,
-//                R.string.country_fried_steak_and_egg,
-//                R.string.CFSE_description,
-//                R.string.distance,
-//                new Restaurant()));
-//
-//        dishList.add(new Dish(R.drawable.chickenshawarma,
-//                R.string.ChickenShawarma,
-//                R.string.ChickenShawarmaDescription,
-//                R.string.distance,
-//                new Restaurant())
-//        );
-    }
+//    public void initializeDishList() {
+//        // hardcoded for now but will be changed into random auto-generated lists
+////        dishList.add(new Dish(R.drawable.countryfriedsteak,
+////                R.string.country_fried_steak_and_egg,
+////                R.string.CFSE_description,
+////                R.string.distance,
+////                new Restaurant()));
+////
+////        dishList.add(new Dish(R.drawable.chickenshawarma,
+////                R.string.ChickenShawarma,
+////                R.string.ChickenShawarmaDescription,
+////                R.string.distance,
+////                new Restaurant())
+////        );
+//    }
 
     /**
      * Initializes the default profile page.
      */
     //TODO: Should display a random dish selected from dishList
     public void initializeDefaultProfilePage() {
-        Dish dish = dishList.get(0);
+        //TODO: change dishListTemp to dishList
+        Random ran = new Random();
+        int i = ran.nextInt(dishListTemp.size());
+        Dish dish = dishListTemp.get(i);
+        currentDish = dish;
         setProfile(dish);
     }
 
@@ -114,17 +200,55 @@ public class MainProfile extends AppCompatActivity {
             case MotionEvent.ACTION_UP:
                 x2 = touchEvent.getX();
                 y2 = touchEvent.getY();
-
-                if (x1 > x2) { // swiping left
-                    setProfile(dishList.get(1));
-                } else if (x1 < x2) { // swiping right
-                    Intent i = new Intent(MainProfile.this, MatchPage.class);
-                    //TODO: set up new MatchPage UI & Match List page
-                    startActivity(i);
+                if (Math.abs(x1 - x2)>30) {
+                    System.out.println("swipe detected");
+                    if (x1 > x2) { // swiping left
+                        dislikeCurrentDish();
+                    } else if (x1 < x2) { // swiping right
+                        likeCurrentDish();
+                    }
                 }
 
                 break;
         }
         return false;
+    }
+
+    /**
+     * Deletes current dish from dishList, adds current dish to swipeLeftlist,
+     * and load a new dish to display
+     */
+    public void dislikeCurrentDish() {
+        swipeLeftDishes.add(currentDish);
+        dishListTemp.remove(currentDish);
+        setProfile(dishListTemp.get(0));
+        System.out.println("--------------------------------Current dish disliked");
+        System.out.println(likedDishes);
+        System.out.println(swipeLeftDishes);
+    }
+
+    /**
+     * Moves to the new activity, add current dish to likedList,
+     * and load a new dish to display
+     * TODO: feel free to add more features to swiping right as long as you think it's necessary
+     */
+    public void likeCurrentDish() {
+
+        likedDishes.add(currentDish);
+        dishListTemp.remove(currentDish);
+        setProfile(dishListTemp.get(0));
+        System.out.println("--------------------------------Current dish liked");
+        System.out.println(likedDishes);
+        System.out.println(swipeLeftDishes);
+        Intent i = new Intent(MainProfile.this, MatchPage.class);
+
+        //TODO: set up new MatchPage UI & Match List page
+        startActivity(i);
+
+    }
+
+    //helper methods
+    public String convertStrIdtoString(int ID) {
+        return getString(ID);
     }
 }
