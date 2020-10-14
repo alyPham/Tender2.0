5d8a20b7cb1e24780759d387d5dd45eed590cda3
Jeongyeob Hong <jhong2@macalester.edu>
2020-10-13 21:21:42 -0500

    Login Features(Working on)

---
+++ app/src/main/java/com/example/tender/LoginOrSignUp.java
@@ -0,0 +1,41 @@
+package com.example.tender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginOrSignUp extends AppCompatActivity {
+
+    private Button user_Login, user_SignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_or_signup);
+
+        user_Login = (Button) findViewById(R.id.button_login);
+        user_SignUp = (Button) findViewById(R.id.button_signup);
+
+        user_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrSignUp.this, MainProfile.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        user_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrSignUp.this, SignUp.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
