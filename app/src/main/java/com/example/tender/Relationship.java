4f3398cb025cad58f37bbdb01e5cc3ef9094205d
alyPham <apham3@macalester.edu>
2020-10-18 22:37:01 -0500

    Relationship updated and linked to firebase!

--- app/src/main/java/com/example/tender/Relationship.java
+++ app/src/main/java/com/example/tender/Relationship.java
@@ -1,18 +1,21 @@
 package com.example.tender;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Relationship extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     boolean longTerm = false;
     boolean hookUp = false;
-
     Button longButton;
     Button shortButton;
 
@@ -23,27 +26,49 @@ public class Relationship extends AppCompatActivity {
 
         longButton = findViewById(R.id.longButton);
         shortButton = findViewById(R.id.shortButton);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
     public void setLongTerm(View view){
-        if(!longTerm && !hookUp){
+        if(!longTerm && !hookUp) {
             longTerm = true;
-            longButton.setBackgroundColor(Color.parseColor("#F86868"));
+            longButton.setBackground(getResources().getDrawable(R.drawable.rounded_button_2));
+            mDatabase.child("profile").child("user1").child("relationship").child("longTerm").setValue("y");
         }
-        if(longTerm){
+        else if(longTerm && !hookUp){
             longTerm = false;
-            longButton.setBackgroundColor(Color.parseColor("#FF8B8B"));
+            longButton.setBackground(getResources().getDrawable(R.drawable.rounded_button));
+            mDatabase.child("profile").child("user1").child("relationship").child("longTerm").setValue("n");
+        }
+        else if(hookUp){
+            longTerm = true;
+            hookUp = false;
+            longButton.setBackground(getResources().getDrawable(R.drawable.rounded_button_2));
+            shortButton.setBackground(getResources().getDrawable(R.drawable.rounded_button));
+            mDatabase.child("profile").child("user1").child("relationship").child("longTerm").setValue("y");
+            mDatabase.child("profile").child("user1").child("relationship").child("hookUp").setValue("n");
         }
     }
 
     public void setShortTerm(View view){
-        if(!longTerm && !hookUp){
+        if(!longTerm && !hookUp) {
             hookUp = true;
-            shortButton.setBackgroundColor(Color.parseColor("#F86868"));
+            shortButton.setBackground(getResources().getDrawable(R.drawable.rounded_button_2));
+            mDatabase.child("profile").child("user1").child("relationship").child("hookUp").setValue("y");
         }
-        if(hookUp){
+        else if(!longTerm && hookUp){
             hookUp = false;
-            shortButton.setBackgroundColor(Color.parseColor("#FF8B8B"));
+            shortButton.setBackground(getResources().getDrawable(R.drawable.rounded_button));
+            mDatabase.child("profile").child("user1").child("relationship").child("hookUp").setValue("n");
+        }
+        else if(longTerm){
+            hookUp = true;
+            longTerm = false;
+            shortButton.setBackground(getResources().getDrawable(R.drawable.rounded_button_2));
+            longButton.setBackground(getResources().getDrawable(R.drawable.rounded_button));
+            mDatabase.child("profile").child("user1").child("relationship").child("longTerm").setValue("n");
+            mDatabase.child("profile").child("user1").child("relationship").child("hookUp").setValue("y");
         }
     }
 
