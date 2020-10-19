f32213e150ca80c2d7b87c842e4601985d4bd5fb
Jeongyeob Hong <jhong2@macalester.edu>
2020-10-18 19:39:28 -0500

    Login Features (Completed)

---
+++ app/src/main/java/com/example/tender/UserInfo.java
@@ -0,0 +1,14 @@
+package com.example.tender;
+
+public class UserInfo {
+    public String email, password;
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+}
