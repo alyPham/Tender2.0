a40185d3d467204e587fd6fdf4b7bd593044a209
Jeongyeob Hong <jhong2@macalester.edu>
2020-10-18 19:36:37 -0500

    Login Features (Completed)

--- app/src/main/java/com/example/tender/Login.java
+++ app/src/main/java/com/example/tender/Login.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,9 +22,9 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     private Button mLogin;
     private EditText mEmail, mPassword;
+    private ProgressBar mProgressbar;
 
 
     @Override
@@ -29,23 +32,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        //      Updating the firebase
-        mAuth = FirebaseAuth.getInstance();
-        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-//              If user sign up then go to dietary page
-                if (user != null){
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return;
-                }
-
-            }
-        };
 
         mLogin = (Button) findViewById(R.id.login_button);
 
@@ -53,17 +39,54 @@ public class Login extends AppCompatActivity {
 
         mPassword = (EditText) findViewById(R.id.password);
 
-//      If user click register, add the new user in fire base. If not, print out an error message
+        mProgressbar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        //User Click the button everything starts...
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
+                final String email = mEmail.getText().toString().trim();
+                final String password = mPassword.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    mEmail.setError("Email is required");
+                    mEmail.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmail.setError("Please provide valid a email address");
+                    mEmail.requestFocus();
+                    return;
+                }
+
+                if (password.isEmpty()){
+                    mPassword.setError("Password is required");
+                    mPassword.requestFocus();
+                    return;
+                }
+
+                if (password.length() < 6){
+                    mPassword.setError("Password is at least 6 characters!");
+                    mPassword.requestFocus();
+                    return;
+                }
+                mProgressbar.setVisibility(View.VISIBLE);
+
                 mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()){
-                            Toast.makeText(Login.this, "Login Error", Toast.LENGTH_SHORT);
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+                            mProgressbar.setVisibility(View.GONE);
+                            finish();
+                        } else {
+                            Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                            mProgressbar.setVisibility(View.GONE);
                         }
                     }
                 });
@@ -71,18 +94,5 @@ public class Login extends AppCompatActivity {
             }
         });
 
-    }
-    //  To make sure firebase listener starts when activity starts
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(firebaseAuthStateListener);
-    }
-
-    //    To make sure firebase listener stops when activity stops
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mAuth.removeAuthStateListener(firebaseAuthStateListener);
     }
 }
