7f437bef35941d411fd021cf987b4b9ac0080be1
alyPham <apham3@macalester.edu>
2020-10-21 19:10:44 -0500

    match display?????? but maybe better

--- app/src/main/java/com/example/tender/MatchDisplay.java
+++ app/src/main/java/com/example/tender/MatchDisplay.java
@@ -3,9 +3,14 @@ package com.example.tender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,12 +21,11 @@ public class MatchDisplay extends AppCompatActivity {
     private static final String TAG = "TAG";
     ImageButton home;
     ImageButton back;
-    MainActivity mainActivity;
-    String names;
     TextView dish1, dish2, dish3, dish4;
+    ImageView image1, image2, image3, image4;
     int counter = 0;
-    List<Dish> likeList;
-    String[] value;
+    String[] dishNames;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +33,21 @@ public class MatchDisplay extends AppCompatActivity {
         setContentView(R.layout.activity_match_page);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            value = extras.getStringArray("key");
-            //The key argument here must match that used in the other activity
+            dishNames = extras.getStringArray("key");
         }
-        Toast.makeText(this, "matched!", Toast.LENGTH_SHORT).show();
+
         home = findViewById(R.id.home_matched);
         back = findViewById(R.id.back_button4);
 
-//        if (getIntent().hasExtra("matchedList")) { // prevents NullPointerException
-//            getIntent().getExtra
-//        }
+        dish1 = findViewById(R.id.dishName1);
+        dish2 = findViewById(R.id.dishName2);
+        dish3 = findViewById(R.id.dishName3);
+        dish4 = findViewById(R.id.dishName4);
 
-        dish1 = findViewById(R.id.Dish1);
-        dish2 = findViewById(R.id.Dish2);
-        dish3 = findViewById(R.id.Dish3);
-        dish4 = findViewById(R.id.Dish4);
-
-//
-
+        image1 = findViewById(R.id.dishImage1);
+        image2 = findViewById(R.id.dishImage2);
+        image3 = findViewById(R.id.dishImage3);
+        image4 = findViewById(R.id.dishImage4);
 
         setText();
 
@@ -62,22 +63,42 @@ public class MatchDisplay extends AppCompatActivity {
 
     }
 
-    public void setText(){
-        for(String dish: value){
+    public void setText() {
+        for (String dish : dishNames) {
             counter++;
-            if(counter == 1){
-            dish1.setText(dish);
-        }
-            if(counter == 2){
-            dish2.setText(dish);
-        }
-            if(counter == 3){
-            dish3.setText(dish);
-        }
-            if(counter == 4){
-            dish4.setText(dish);
-            counter = 0;
-        }
+            if (counter == 1) {
+                dish1.setText(dish);
+                Resources res = getResources();
+                String mDrawableName = "Resized " + dish;
+                int resID = res.getIdentifier(mDrawableName , "drawable", getPackageName());
+                Drawable drawable = res.getDrawable(resID );
+                image1.setImageDrawable(drawable);
+            }
+            if (counter == 2) {
+                dish2.setText(dish);
+                Resources res = getResources();
+                String mDrawableName = "Resized " + dish;
+                int resID = res.getIdentifier(mDrawableName , "drawable", getPackageName());
+                Drawable drawable = res.getDrawable(resID );
+                image2.setImageDrawable(drawable);
+            }
+            if (counter == 3) {
+                dish3.setText(dish);
+                Resources res = getResources();
+                String mDrawableName = "Resized " + dish;
+                int resID = res.getIdentifier(mDrawableName , "drawable", getPackageName());
+                Drawable drawable = res.getDrawable(resID );
+                image3.setImageDrawable(drawable);
+            }
+            if (counter == 4) {
+                dish4.setText(dish);
+                Resources res = getResources();
+                String mDrawableName = "Resized " + dish;
+                int resID = res.getIdentifier(mDrawableName , "drawable", getPackageName());
+                Drawable drawable = res.getDrawable(resID );
+                image4.setImageDrawable(drawable);
+                counter = 0;
+            }
         }
 
     }
