8c51f081f9bdcaefea6bb32e80605b1091182710
alyPham <apham3@macalester.edu>
2020-10-22 21:39:49 -0500

    match display?????? but maybe better pt.2

--- app/src/main/java/com/example/tender/UserSettingsPage.java
+++ app/src/main/java/com/example/tender/UserSettingsPage.java
@@ -26,42 +26,50 @@ import java.util.function.Function;
 
 
 public class UserSettingsPage extends AppCompatActivity {
-    private DatabaseReference mDatabase;
-    private UserInfo currentUserInfo;
+//    private DatabaseReference mDatabase;
+//    private UserInfo currentUserInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_new_user);
-        currentUserInfo = new UserInfo();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-//        try {
-//            retrieveUserData((l)->{
-//    //            System.out.println(l.getV());
-//    //            if (currentUserInfo.getV() != null) {
-//    //                System.out.println("--------------------------v " + v);
-//    //            }
-//                return null;
-//            });
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+//        currentUserInfo = new UserInfo();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+////        try {
+////            retrieveUserData((l)->{
+////    //            System.out.println(l.getV());
+////    //            if (currentUserInfo.getV() != null) {
+////    //                System.out.println("--------------------------v " + v);
+////    //            }
+////                return null;
+////            });
+////        } catch (IOException e) {
+////            e.printStackTrace();
+////        }
     }
 
-    public void dietPage(View view){
-        Intent i = new Intent(UserSettingsPage.this, DietaryPage.class);
-        startActivity(i);
-    }
+    /**
+     * Unable to implement
+     * @param view
+     */
+//    public void dietPage(View view){
+//        Intent i = new Intent(UserSettingsPage.this, DietaryPage.class);
+//        startActivity(i);
+//    }
 
-    public void locationPage(View view) {
+    public void gettingStartedPage(View view) {
         Intent i = new Intent(UserSettingsPage.this, GettingStartedPage.class);
         startActivity(i);
     }
 
-    public void relationshipPage(View view) {
-        Intent i = new Intent(UserSettingsPage.this, Relationship.class);
-        startActivity(i);
-    }
+    /**
+     * Unable to implement
+     * @param view
+     */
+//    public void relationshipPage(View view) {
+//        Intent i = new Intent(UserSettingsPage.this, Relationship.class);
+//        startActivity(i);
+//    }
 
     public void goToFood(View view) {
         Intent i = new Intent(UserSettingsPage.this, MainActivity.class);
